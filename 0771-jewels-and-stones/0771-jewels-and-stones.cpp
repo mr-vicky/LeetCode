a52d92f4889eae77938cc59bfc353e605fe34896
V____K <86181045+mr-vicky@users.noreply.github.com>
2023-02-12 23:50:02 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (80.16%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int count = 0;
+        int n = jewels.length();
+        int m = stones.length();
+    
+        for(int i=0; i<n; i++)
+            for(int j=0; j<m; j++)
+                if(jewels[i] == stones[j])
+                    count++;
+                    
+        return count;
+    }
+};
