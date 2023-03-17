259339b2d8c610be677342c29410a972c659eeff
V____K <86181045+mr-vicky@users.noreply.github.com>
2023-03-17 14:19:24 +0530

    Time: 330 ms (21.54%), Space: 80.1 MB (43.90%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minSetSize(vector<int>& arr) {
+        int n = arr.size();
+        map<int, int>mp;
+        vector<int>v;
+        int count = 0;
+        int sum = 0;
+        
+        for(int i=0; i<n; i++)
+            mp[arr[i]] = mp[arr[i]]+1;
+
+        for(auto x: mp) 
+            v.push_back(x.second);
+        
+        sort(v.begin(), v.end(), greater<int>());
+
+        for(auto x: v)
+        {
+            sum += x;
+            count++;
+            if(sum>=n/2)
+                return count;
+        }
+        return 0;
+    }
+};
