7fc5cf0d5148e25597d1dbfa59e398b03c861cec
V____K <86181045+mr-vicky@users.noreply.github.com>
2023-03-28 09:29:48 +0530

    Time: 7 ms, Memory: 9.2 MB - LeetHub

---
+++ merge-sorted-array/merge-sorted-array.cpp
@@ -0,0 +1,16 @@
+// TC: O((m+n)*log(m+n))
+// SC: O(m+n)
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int>res;
+        for(int i=0; i<m; i++)
+            res.push_back(nums1[i]);
+
+         for(int i=0; i<n; i++)
+            res.push_back(nums2[i]);
+
+        sort(res.begin(), res.end());
+        nums1=res;
+    }
+};
