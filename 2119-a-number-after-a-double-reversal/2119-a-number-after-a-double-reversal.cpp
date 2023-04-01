d84c84deaf6a1a02566bec06c8a64a1d4cca08bd
V____K <86181045+mr-vicky@users.noreply.github.com>
2023-04-01 10:14:21 +0530

    Time: 3 ms (38.17%), Space: 5.7 MB (96.24%) - LeetHub

---
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isSameAfterReversals(int num) {
+        int reversed1 = 0;
+        int reversed2 = 0;
+        int temp = num;
+
+        while(num>0)
+        {
+            reversed1 = reversed1*10 + num%10;
+            num = num/10;
+        }
+
+        while(reversed1>0)
+        {
+            reversed2 = reversed2*10 + reversed1%10;
+            reversed1 = reversed1/10;
+        }
+        return reversed2==temp;
+    }
+};
