fdd7270f314919ad938f477ff2572b002088eced
V____K <86181045+mr-vicky@users.noreply.github.com>
2023-05-01 23:32:16 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (84.06%) - LeetHub

---
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    double average(vector<int>& salary) {
+        int n = salary.size();
+        double sum = 0;
+
+        sort(salary.begin(), salary.end());
+        for(int i=1; i<n-1; i++)
+            sum += salary[i];
+            
+        return sum/(n-2);
+    }
+};
