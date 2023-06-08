f6359bc885c0138a6af71d3fb9b9856e3e839ea1
Task_Master <86181045+mr-vicky@users.noreply.github.com>
2023-06-08 23:28:48 +0530

    Time: 88 ms (25.89%), Space: 30.8 MB (64.94%) - LeetHub

--- 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.cpp
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.cpp
@@ -3,20 +3,17 @@ public:
     int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
         int count = 0;
        
-        if(ruleKey == "type")
-        {
+        if(ruleKey == "type"){
             for(int i=0; i<items.size(); i++)
                 if(items[i][0]==ruleValue)
                     count++;
         }
-        else if(ruleKey == "color")
-        {
+        else if(ruleKey == "color"){
             for(int i=0; i<items.size(); i++)
                 if(items[i][1]==ruleValue)
                     count++;
         }
-        else
-        {
+        else{
             for(int i=0; i<items.size(); i++)
                 if(items[i][2]==ruleValue)
                     count++;
