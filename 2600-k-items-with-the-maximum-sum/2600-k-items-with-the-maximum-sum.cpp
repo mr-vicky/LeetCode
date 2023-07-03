de95a7a5f0bab223540ed58e3332c00ebb2ffb0a
V____K <86181045+mr-vicky@users.noreply.github.com>
2023-07-03 23:44:00 +0530

    Time: 7 ms (18.07%), Space: 6.6 MB (8.54%) - LeetHub

--- 2600-k-items-with-the-maximum-sum/2600-k-items-with-the-maximum-sum.cpp
+++ 2600-k-items-with-the-maximum-sum/2600-k-items-with-the-maximum-sum.cpp
@@ -4,19 +4,18 @@ public:
         int max_sum = 0;
         vector<int>v;
 
-        while(numOnes--){
+        while(numOnes--)
             v.push_back(1);
-        }
-        while(numZeros--){
-            v.push_back(0);
-        }
-        while(numNegOnes--){
-            v.push_back(-1);
-        }
 
-        for(int i=0; i<k; i++){
+        while(numZeros--)
+            v.push_back(0);
+            
+        while(numNegOnes--)
+            v.push_back(-1);
+
+        for(int i=0; i<k; i++)
             max_sum += v[i]; 
-        }
+        
         return max_sum;
     }
 };
