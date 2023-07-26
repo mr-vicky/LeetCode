0a691b47ab6e5b6d3f13eeb08aae20bbbd72edb9
V____K <86181045+mr-vicky@users.noreply.github.com>
2023-07-26 15:54:22 +0530

    Added solution - LeetHub

---
+++ Addition of two square matrices - GFG/addition-of-two-square-matrices.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+public:
+    void Addition(vector<vector<int> >& matrixA, vector<vector<int>>&matrixB) {
+        // Code here
+        int m=matrixA.size();
+        int n=matrixB.size();
+        
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                matrixA[i][j]+=matrixB[i][j];
+            }
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<vector<int>> matrixA(n, vector<int>(n,0));
+		vector<vector<int>> matrixB(n, vector<int>(n,0));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				cin >> matrixA[i][j];
+			}
+		}
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				cin >> matrixB[i][j];
+			}
+		}
+		Solution ob;
+		ob.Addition(matrixA, matrixB);
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++)
+				cout << matrixA[i][j] <<" ";
+			cout << "\n";
+		}
+	}
+	return 0;
+}
+// } Driver Code Ends
