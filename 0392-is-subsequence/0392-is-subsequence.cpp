0f6ec0bccf380bd30b69c79058e3eec87a8a4eb0
Vivek Sarade <86181045+mr-vicky@users.noreply.github.com>
2023-09-04 21:16:39 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (93.29%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,28 +1,25 @@
-// Recursive Approach
+// Method 2: Recursive Approach
 // TC: O(m+n)
 // SC: O(m+n)
+
 class Solution {
     int totalSubsequenceChar(string &s, string &t, int m, int n){
-        if(m==0 || n==0){
+        if(m==0 || n==0)
             return false;
-        }
-        if(s[m-1]==t[n-1]){
+        if(s[m-1]==t[n-1])
             return 1+totalSubsequenceChar(s, t, m-1, n-1);
-        }
-        else{
+        else
             return totalSubsequenceChar(s, t, m, n-1);
-        }
     }
 
 public:
     bool isSubsequence(string s, string t) {
         int m=s.length();
         int n=t.length();
-        if(totalSubsequenceChar(s, t,  m, n)==m){
+        
+        if(totalSubsequenceChar(s, t,  m, n)==m)
             return true;
-        }
-        else{
+        else
             return false;
-        }
     }
 };
