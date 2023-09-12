d64a15e2c3a4048b21d584adf7ff86ac30a615ac
Vivek Sarade <86181045+mr-vicky@users.noreply.github.com>
2023-09-12 16:13:04 +0530

    Time: 4 ms (83.96%), Space: 5.9 MB (51.30%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,28 +1,21 @@
-// TC: O(n) 
-// SC: O(n)
-// where, n = no. of digits in x
+// Method 2: 
+// TC: O(n)
+// SC: O(1)
+
 class Solution {
 public:
     bool isPalindrome(int x) {
-        vector<int>v1;
-        vector<int>v2;
-        if(x<0) 
-            return false;
+        string s = to_string(x);
+        int i = 0;
+        int j = s.length()-1;
         
-        while(x>0)
-        {
-            int temp = x%10;
-            v1.push_back(temp);
-            x/=10;
+        while( i < j ){
+            if(s[i] != s[j]){
+                return false;
+            }
+            i++;
+            j--;
         }
-        
-        for(auto x: v1)
-            v2.push_back(x);
-        
-        reverse(v2.begin(), v2.end());
-        
-        if(v1==v2)  
-            return true;
-        return false;
+        return true;
     }
 };
