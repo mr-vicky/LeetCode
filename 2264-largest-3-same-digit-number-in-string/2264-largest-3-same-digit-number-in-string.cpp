11d35238a4c19202ab725c56d14964dd20229adb
Vivek Sarade <86181045+mr-vicky@users.noreply.github.com>
2023-12-04 10:36:08 +0530

    Time: 4 ms (47.83%), Space: 7 MB (18.84%) - LeetHub

--- 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
@@ -1,30 +1,28 @@
+#include<typeinfo>
 class Solution {
 public:
     string largestGoodInteger(string num) {
-        vector<int>temp;
-        int n = num.size();
-
-        for(int i=0; i<n-2; i++){
-            if(num[i]==num[i+1] && num[i]==num[i+2]){
-                temp.push_back(num[i] - '0');
-                temp.push_back(num[i] - '0');
-                temp.push_back(num[i] - '0');
-                i+=2;
-            }    
+        int n = num.length();
+        int curr = 0;
+        int max_good = 0;
+        string good_string = "";
+        bool flag = false;
+        for(int i = 0; i < n-1; i++){
+            curr = 0;
+            if(num[i] == num[i+1] && num[i+1] == num[i+2]){
+                char temp = num[i];
+                curr = (curr * 10) + temp - '0';
+                curr = (curr * 10) + temp - '0';
+                curr = (curr * 10) + temp - '0';
+                max_good = max(max_good, curr);
+                flag = true;
+            }
         }
 
-        sort(temp.begin(), temp.end(), greater<int>());
-        int m = temp.size();
-        
-        if(temp.empty()){
-            return "";
+        if(flag == true){
+            good_string = to_string(max_good);
+            if(max_good == 0) good_string = "000";
         }
-        else{
-            string res;
-            res.push_back(temp[0]+'0');
-            res.push_back(temp[0]+'0');
-            res.push_back(temp[0]+'0');
-            return res;
-        }
+        return good_string;
     }
 };
