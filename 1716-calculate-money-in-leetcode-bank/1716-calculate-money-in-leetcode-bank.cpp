187c9e46913f759a30c4a5e5094e84976ad59007
Vivek Sarade <86181045+mr-vicky@users.noreply.github.com>
2023-12-06 09:43:42 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (70.53%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     int totalMoney(int n) {
-        int count_days = 0;
         int total_money = 0;
-
-        for(int i=0; i<n; i++)
-        {
-           for(int j=i+1; j<=i+7; j++)
-           {
-               total_money += j;
-               count_days++;
-
-               if(count_days==n)
-                    return total_money;
-           }
+        int count_days = 0;
+        
+        for(int i = 1; i <= n; i++){
+            for(int j = i; j < i+7 ; j++){
+                total_money += j;
+                count_days++;
+                if(count_days == n) return total_money;
+            }
         }
-        return -1;
+        return total_money;
     }
 };
