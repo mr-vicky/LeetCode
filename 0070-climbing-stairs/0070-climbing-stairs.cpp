943e7c0eccd6f15b8cd3be3974105d08d25b5bf9
Vivek Sarade <86181045+mr-vicky@users.noreply.github.com>
2024-04-06 21:40:36 +0530

    Time: 2 ms (47.33%), Space: 7.3 MB (48.65%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     int climbStairs(int n) {
         vector<int>nums{1, 2, 3};
-
-        if(n<4) return n;
-        for(int i=3; i<n; i++)
-            nums.push_back(nums[i-1]+nums[i-2]);
-        
+        if(n < 4) return n;
+        for(int i = 3; i < n; i++){
+            nums.push_back(nums[i - 1] + nums[i - 2]);
+        }
         return nums[n-1];
     }
 };
