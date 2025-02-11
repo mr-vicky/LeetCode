45c54b3939d4ee960d0c44f1adc4ee653f4257ff
Vivek Sarade <86181045+mr-vicky@users.noreply.github.com>
2025-02-11 20:46:56 +0530

    Time: 0 ms, Memory: 28.3 MB - LeetHub

--- 20370-706-169-majority-element/20370-706-169-majority-element.cpp
+++ 20370-706-169-majority-element/20370-706-169-majority-element.cpp
@@ -1,11 +1,20 @@
-// Method 1: 
-// TC: O(N*logN)
-// SC: O(1) or O(m) according to sorting algo.
+// Method 2: Hashing
+// TC: O(N)
+// SC: O(N)
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        unordered_map<int, int> count;
         int n = nums.size();
-        sort(nums.begin(), nums.end());
-        return nums[n/2];
+        for(auto n : nums){
+            count[n]++;
+        }
+
+        for(auto x : count){
+            if(x.second > n/2){
+                return x.first;
+            }
+        }
+        return -1;
     }
 };
