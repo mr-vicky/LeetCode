ddd13ad01cf0d9ea8d489b2bb017f1985dccb2ff
Vivek Sarade <86181045+mr-vicky@users.noreply.github.com>
2025-02-15 20:29:53 +0530

    Time: 0 ms, Memory: 11.4 MB - LeetHub

--- 19407-692-75-sort-colors/19407-692-75-sort-colors.cpp
+++ 19407-692-75-sort-colors/19407-692-75-sort-colors.cpp
@@ -1,21 +1,32 @@
+// Method 2: 
 // TC: O(N)
-// SC: O(N)
+// SC: O(1)
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        vector<int>res;
         int n = nums.size();
-        for(auto n : nums){
-            if(n == 0)  res.push_back(0);
-        }        
+        int count_zero = 0;
+        int count_one = 0;
+        int count_two = 0;
 
-        for(auto n : nums){
-            if(n == 1)  res.push_back(1);
-        }   
-
-        for(auto n : nums){
-            if(n == 2)  res.push_back(2);
-        }   
-        nums = res;
+        for(auto num : nums){
+            if(num == 0) count_zero++;
+            if(num == 1) count_one++;
+            if(num == 2) count_two++;
+        }
+        
+        nums.clear();
+        while(count_zero){
+            nums.push_back(0);
+            count_zero--;
+        }
+        while(count_one){
+            nums.push_back(1);
+            count_one--;
+        }
+        while(count_two){
+            nums.push_back(2);
+            count_two--;
+        }
     }
 };
